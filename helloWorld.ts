392b2059009b554b27c88dded927d99f8a3483b4
PedroPK <pedropk@gmail.com>
2022-03-08 20:23:29 -0300

    First HelloWorld in TypeScript

---
+++ helloWorld.ts
@@ -0,0 +1,11 @@
+/**
+ * How to execute
+ * 
+ * 01) Open the folder with the TS file in a Terminal/Console
+ * 02) Execute the following command
+ * 		tsc helloWorld.ts
+ * 03) To execute the compiled (JS) file, use the command below
+ * 		node helloWorld.js
+ */
+const hello:String = 'Hello World';
+console.log(hello);
